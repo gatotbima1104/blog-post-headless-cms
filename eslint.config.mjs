f3123590e4c7887903c583e2c5fb22d24e59108e
Muhamad Gatot Supiadin <gatotbima2002@gmail.com>
2024-12-14 16:47:00 +0700

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
-})
+});
 
 const eslintConfig = [
   ...compat.config({
@@ -13,6 +12,6 @@ const eslintConfig = [
       '@next/next/no-page-custom-font': 'off',
     },
   }),
-]
+];
 
-export default eslintConfig
+export default eslintConfig;
